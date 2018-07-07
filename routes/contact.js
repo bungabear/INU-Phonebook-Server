cbb3631fc9925f51a9f3c5f55136451aafa31db6
Minjae Son <bungabear6422@gmail.com>
2018-07-08 00:47:45 +0900

    하루단위 memoize로 응답속도 향상

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
-/* copyright(c) 2016 All rights reserved by jongwook(koyu1212@naver.com) 201003051 컴퓨터공학부 고종욱
- * thanks for Bbulbum
+/*
+*		201003051 컴퓨터공학부 고종욱 jongwook(koyu1212@naver.com) thanks for Bbulbum
+* 	201301459 컴퓨터공학부 손민재 Monjae Son(bungabear6422@gmail.com)
 */
 const oracledb = require('oracledb');
 const http = require('http');
@@ -7,40 +8,88 @@ const fs = require('fs');
 
 oracledb.outFormat = oracledb.OBJECT
 
-function getcontact(req, res, next) {
-	let sql = 'select nm as name, job_div as position, offi_tel_no as phone, dpt_nm as dpart, univ_nm as part from v_campus_tel_no, v_org where v_campus_tel_no.dpt_cd = v_org.dpt_cd'
-	let bindvar = {}
-	let pool = oracledb.getPool();
-	pool.getConnection((err,connection)=>{
-			connection.execute( sql, bindvar, {maxRows:2000}, (err, results)=> {
-				if(err){
-					next(err);
-					return;
-				}
-				res.header("Content-Type", "application/json; charset=utf-8");
-				res.send(results.rows);
-				res.end();
-				}
-			);
-	});
+var oneMemoize = (func)=>{
+  var cached
+  var oldKey
+  var memoize = function(key){
+    if(oldKey != key){
+      oldKey = key
+      cached = func.apply(this,arguments)
+    }
+		else{
+			console.log('cache hit!')
+		}
+    return cached
+  }
+  return memoize;
 }
 
-function getcontactgroup(req, res, next) {
-	let sql = 'select distinct univ_nm as part from v_campus_tel_no, v_org where v_campus_tel_no.dpt_cd = v_org.dpt_cd'
-	let bindvar = {}
+// -------------------- route contact
+var getContactMemoiz = oneMemoize(_getcontact)
+async function getcontact(req, res, next) {
+	// 하루 단위로 memoize cache 변경
+	let date = new Date()
+	let result = await getContactMemoiz(date.getDay())
+	res.header("Content-Type", "application/json; charset=utf-8");
+	res.send(result);
+	return
+}
+
+async function _getcontact(){
+	let sql = 'select nm as name, job_div as position, offi_tel_no as phone, dpt_nm as dpart, univ_nm as part from v_campus_tel_no, v_org where v_campus_tel_no.dpt_cd = v_org.dpt_cd'
 	let pool = oracledb.getPool();
-	pool.getConnection((err,connection)=>{
-			pool.execute( sql, bindvar, {maxRows:2000}, (err, results)=> {
-				if(err){
-					next(err);
-					return;
-				}
-				res.header("Content-Type", "application/json; charset=utf-8");
-				res.send(results.rows);
-				res.end();
-				}
-			);
-	});
+	try{
+		return await dbExecute(pool, sql)
+	}
+	catch(e){
+		console.error(e)
+		return null
+	}
+}
+
+// -------------------- route contactgroup
+var getContactGropuMemoiz = oneMemoize(_getcontactgroup)
+async function getcontactgroup(req, res, next) {
+	// 하루 단위로 memoize cache 변경
+	let date = new Date()
+	let result = await getContactGropuMemoiz(date.getDay())
+	res.header("Content-Type", "application/json; charset=utf-8");
+	res.send(result);
+	return
+}
+
+async function _getcontactgroup(){
+	let sql = 'select nm as name, job_div as position, offi_tel_no as phone, dpt_nm as dpart, univ_nm as part from v_campus_tel_no, v_org where v_campus_tel_no.dpt_cd = v_org.dpt_cd'
+	let pool = oracledb.getPool();
+	try{
+		return await dbExecute(pool, sql)
+	}
+	catch(e){
+		console.error(e)
+		return null
+	}
+}
+
+// 단 하나만을 기억하는 memoize
+function dbExecute(pool, sql) {
+  return new Promise(async function(resolve, reject) {
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      let result = await conn.execute(sql);
+      resolve(result.rows);
+    } catch (err) { // catches errors in getConnection and the query
+      reject(err);
+    } finally {
+      if (conn) {   // the conn assignment worked, must release
+        try {
+          await conn.release();
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+  });
 }
 
 module.exports.getcontact = getcontact;
